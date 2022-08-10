7c899fd484ee5e6694c40e261c78882111200a04
Johan Sköld <git@johan.skold.cc>
2022-08-09 18:20:07 -0700

    Switching to playerctld

--- configs/sway/playerctl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Specialized script for prioritized media focus control
-
-CHROMIUMS=$(playerctl --list-all | grep chromium. | xargs | sed 's/ /,/g')
-FIREFOXES=$(playerctl --list-all | grep firefox. | xargs | sed 's/ /,/g')
-playerctl --player spotify,$CHROMIUMS,$FIREFOXES $@
